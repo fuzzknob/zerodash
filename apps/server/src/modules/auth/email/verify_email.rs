329b5a1ff175504ece9e2140c166ac4e9c10c760
Gagan Rai <gagan_rai@outlook.com>
2024-10-28 01:15:22 +0545

    feat: add better email template

--- apps/server/src/modules/auth/email/verify_email.rs
+++ apps/server/src/modules/auth/email/verify_email.rs
@@ -1,24 +1,24 @@
 use lunarus::{mail::Mail, Result};
 use serde::Serialize;
 
-static TEMPLATE: &str = "
-Dear {{name}},
-
-Thank you for creating an account with us. To complete the registration process, we need to verify your email address.
-
-Please click the link below to verify your email address:
-
-<a href=\"{{verification_link}}\">Verify Email Address</a>
-
-If you have any issues with the verification link, you can copy and paste the following URL into your browser:
-
-<a href=\"{{verification_link}}\">{{verification_link}}</a>
-
-Once you've verified your email address, you'll be able to access all the features of our platform.
-
-Best regards,
-Gagan from zerodash
-";
+static TEMPLATE: &str = r#"
+<!DOCTYPE html>
+<html>
+    <body style="font-family: Inter, Arial, sans-serif">
+        <p>Hi {{name}},</p>
+        <p>Thank you for creating an account with us. To complete the registration process, we need to verify your email address.</p>
+        <p>Please click the link below to verify your email address:</p>
+        <a href="{{verification_link}}">Verify Email Address</a>
+        <p>If you have any issues with the verification link, you can copy and paste the following URL into your browser:</p>
+        <a href="{{verification_link}}">{{verification_link}}</a>
+        <p>Once you've verified your email address, you'll be able to access all the features of our platform.</p>
+        <p>
+            Kind regards, <br />
+            Gagan from <del>Zero</del>
+        </p>
+    </body>
+</html>
+"#;
 
 #[derive(Debug, Serialize)]
 pub struct VerifyEmailArgs {
